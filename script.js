62e97ca869e5b17b19b874a078759f9a94576794
beepedro <pedro.witkovski@escola.pr.gov.br>
2026-09-18 10:59:11 -0300

    alterei

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+cont botoeCurtir = document.querySelectorAll(".curtir");
+botoesCurtir.forEach(function(botaoCurtir){
+let curtir = fale;
+botaoCurtir.addEventListener("clik", curtir);
+function curtir(){
+    cont contador = botaoCurtir.querySelector("span");
+    if(curtir === false){
+        contador.textContent++;
+        curtir = true;}
+        else{
+            contador.textContent--;
+            curtiu = false;
+      }
+}
+});
